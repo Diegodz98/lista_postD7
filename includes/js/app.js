a79343c5a867ff8f0180c4526ca6b4e62bfd0b00
Diegodz98 <diego.blaz98@gmail.com>
2021-07-18 12:02:33 -0500

    terminando ajustes

--- includes/js/app.js
+++ includes/js/app.js
@@ -4,52 +4,53 @@ document.addEventListener('DOMContentLoaded', (event) => {
     /*---------------
     #Variable
     ----------------*/
-    const inputFiltro = document.querySelector('#filtro');
-    const tabla= document.querySelectorAll('.fila-post');
-    const btnExportar = document.querySelector("#btnExportar")
-
-
-
-
-    //Eventos del dom
-    const eventosDom = ()=>{
-        console.log('hola')
-        inputFiltro.addEventListener('keyup', e=> filtrarPost(e))
-        btnExportar.addEventListener('click', e => exportarExel(e))
-
-    }
-
-
-    //Funciones
-    const filtrarPost = (e)=>{
-       // e.target.value
-       tabla.forEach(el => {
-           el.textContent.toLowerCase().includes(e.target.value) 
-            ? el.classList.remove('filter')
-            : el.classList.add('filter')
-
-       });
-    }
-
-    const exportarExel = (e)=>{
-        e.preventDefault();
-        const $tabla = document.querySelector("#tablaPosts");
-
-        let tableExport = new TableExport($tabla, {
-            exportButtons: false, // No queremos botones
-            filename: "Lista de Post", //Nombre del archivo de Excel
-            sheetname: "posts", //Título de la hoja
-        });
-        let datos = tableExport.getExportData();
+    if(document.querySelector('#filtro')){
+        const inputFiltro = document.querySelector('#filtro');
+        const tabla= document.querySelectorAll('.fila-post');
+        const btnExportar = document.querySelector("#btnExportar")
     
-        let preferenciasDocumento = datos.tablaPosts.xlsx;
-        tableExport.export2file(preferenciasDocumento.data, preferenciasDocumento.mimeType, preferenciasDocumento.filename, preferenciasDocumento.fileExtension, preferenciasDocumento.merges, preferenciasDocumento.RTL, preferenciasDocumento.sheetname);
+    
+    
+    
+        //Eventos del dom
+        const eventosDom = ()=>{
+            inputFiltro.addEventListener('keyup', e=> filtrarPost(e))
+            btnExportar.addEventListener('click', e => exportarExel(e))
+    
+        }
+    
+    
+        //Funciones
+        const filtrarPost = (e)=>{
+           tabla.forEach(el => {
+               el.textContent.toLowerCase().includes(e.target.value) 
+                ? el.classList.remove('filter')
+                : el.classList.add('filter')
+    
+           });
+        }
+    
+        const exportarExel = (e)=>{
+            e.preventDefault();
+            const $tabla = document.querySelector("#tablaPosts");
+    
+            let tableExport = new TableExport($tabla, {
+                exportButtons: false, // No queremos botones
+                filename: "Lista de Post", //Nombre del archivo de Excel
+                sheetname: "posts", //Título de la hoja
+            });
+            let datos = tableExport.getExportData();
+        
+            let preferenciasDocumento = datos.tablaPosts.xlsx;
+            tableExport.export2file(preferenciasDocumento.data, preferenciasDocumento.mimeType, preferenciasDocumento.filename, preferenciasDocumento.fileExtension, preferenciasDocumento.merges, preferenciasDocumento.RTL, preferenciasDocumento.sheetname);
+        }
+    
+    
+    
+        eventosDom()
+    
     }
-
-
-
-    eventosDom()
-
+    
 
 });
 
